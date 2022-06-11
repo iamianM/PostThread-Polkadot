9fb1b7f501146f78f77e7cf7d66c505554c06427
Kevin Kulik <kvnkulik@gmail.com>
2022-06-10 21:59:23 -0400

    Added CSS to Message.css

--- frontend/src/postthread/components/Message.js
+++ frontend/src/postthread/components/Message.js
@@ -4,11 +4,12 @@ import "./css/Message.css"
 export default function Message({ message }) {
     return (
         <div className="message-container">
+            <div className="sub-message-container">
             <h1 className="message-title">{message.title}</h1>
             <p className="message-body">{message.body}</p>
             {/* <Link href={`/posts/${encodeURIComponent(message.id)}`}>
                 <a>Comments</a>
             </Link> */}
-        </div>
+        </div></div>
     )
 }
