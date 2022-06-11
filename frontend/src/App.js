c9000bd3af0bd16ed95c88912a871511e7364926
Ian McCann <mcian91@gmail.com>
2022-06-11 15:49:23 -0700

    added styling and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { createRef } from 'react'
+import React from "react";
+import { createRef } from "react";
 import {
-  Container,
+  // Container,
   Dimmer,
   Loader,
   Grid,
   Sticky,
   Message,
-} from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-import { SubstrateContextProvider } from './substrate-lib'
-import { useSubstrateState } from './substrate-lib'
+import { SubstrateContextProvider } from "./substrate-lib";
+import { useSubstrateState } from "./substrate-lib";
 
-import { DeveloperConsole } from './substrate-lib/components'
+import { DeveloperConsole } from "./substrate-lib/components";
 
-import AccountSelector from './AccountSelector'
+import AccountSelector from "./AccountSelector";
 // import Balances from './Balances'
 // import BlockNumber from './BlockNumber'
 // import Events from './Events'
@@ -24,23 +24,26 @@ import AccountSelector from './AccountSelector'
 // import NodeInfo from './NodeInfo'
 // import TemplateModule from './TemplateModule'
 // import Blogchain from './Blogchain'
-import ListMessages from './postthread/components/ListMessages'
-import CreateMessage from './postthread/components/CreateMessage'
-import CreateMsa from './postthread/components/CreateMsa'
-import RetrieveMsa from './postthread/components/RetrieveMsa'
-import CreateSchema from './postthread/components/CreateSchema'
-import CreateMessageHTML from './postthread/components/CreateMessageHTML'
+import ListMessages from "./postthread/components/ListMessages";
+import CreateMessage from "./postthread/components/CreateMessage";
+import CreateMsa from "./postthread/components/CreateMsa";
+import RetrieveMsa from "./postthread/components/RetrieveMsa";
+import CreateSchema from "./postthread/components/CreateSchema";
+import CreateMessageHTML from "./postthread/components/CreateMessageHTML";
 
-function Main() {
-  const { apiState, apiError, keyringState, currentAccount } = useSubstrateState()
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-  const loader = text => (
+function Main(props) {
+  const { apiState, apiError, keyringState, currentAccount } =
+    useSubstrateState();
+
+  const loader = (text) => (
     <Dimmer active>
       <Loader size="small">{text}</Loader>
     </Dimmer>
-  )
+  );
 
-  const message = errObj => (
+  const message = (errObj) => (
     <Grid centered columns={2} padded>
       <Grid.Column>
         <Message
@@ -52,92 +55,49 @@ function Main() {
         />
       </Grid.Column>
     </Grid>
-  )
+  );
 
-  if (apiState === 'ERROR') return message(apiError)
-  else if (apiState !== 'READY') return loader('Connecting to Substrate')
+  if (apiState === "ERROR") return message(apiError);
+  else if (apiState !== "READY") return loader("Connecting to Substrate");
 
-  if (keyringState !== 'READY') {
+  if (keyringState !== "READY") {
     return loader(
       "Loading accounts (please review any extension's authorization)"
-    )
+    );
   }
 
-  const contextRef = createRef()
-
-  // return (
-  //   <div ref={contextRef}>
-  //     <Sticky context={contextRef}>
-  //       <AccountSelector />
-  //     </Sticky>
-  //     <Container>
-  //       <Grid stackable columns="equal">
-  //         <Grid.Row stretched>
-  //           <NodeInfo />
-  //           <Metadata />
-  //           <BlockNumber />
-  //           <BlockNumber finalized />
-  //         </Grid.Row>
-  //         <Grid.Row>
-  //           <Blogchain />
-  //         </Grid.Row>
-  //         <Grid.Row stretched>
-  //           <Balances />
-  //         </Grid.Row>
-  //         <Grid.Row>
-  //           <Interactor />
-  //           <Events />
-  //         </Grid.Row>
-  //         <Grid.Row>
-  //           <TemplateModule />
-  //         </Grid.Row>
-  //       </Grid>
-  //     </Container>
-  //     <DeveloperConsole />
-  //   </div>
-  // )
+  const contextRef = createRef();
 
   return (
     <div ref={contextRef}>
       <Sticky context={contextRef}>
         <AccountSelector />
       </Sticky>
-      <Container>
-        <Grid divided='vertically'>
-          <Grid.Row columns={2}>
-            <Grid.Column>
-              <ListMessages />
-            </Grid.Column>
-            <Grid.Column>
-              <Grid.Row>
-                <CreateMessage />
-              </Grid.Row>
-              <Grid.Row>
-                <CreateMessageHTML />
-              </Grid.Row>
-              <Grid.Row>
-                <CreateMsa />
-              </Grid.Row>
-              <Grid.Row>
-                {currentAccount && <RetrieveMsa />}
-              </Grid.Row>
-              <Grid.Row>
-                <CreateSchema />
-              </Grid.Row>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      {props.name == "" ? (
+        <ListMessages />
+      ) : props.name == "submit" ? (
+        [<CreateMessage />, <CreateMessageHTML />]
+      ) : props.name == "testing" ? (
+        [currentAccount && <RetrieveMsa />, <CreateMsa />, <CreateSchema />]
+      ) : (
+        ""
+      )}
       <DeveloperConsole />
     </div>
-  )
-
+  );
 }
 
 export default function App() {
   return (
     <SubstrateContextProvider>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main name="" />} />
+          <Route path="/user" element={<Main name="user" />} />
+          <Route path="/submit" element={<Main name="submit" />} />
+          <Route path="/testing" element={<Main name="testing" />} />
+        </Routes>
+      </BrowserRouter>
     </SubstrateContextProvider>
-  )
+  );
 }
--- frontend/src/postthread/components/Message.js
+++ frontend/src/postthread/components/Message.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import "./css/Message.css"
+import React from "react";
+import "./css/Message.css";
 
 export default function Message({ message }) {
-    return (
-        <div className="message-container">
-            <div className="sub-message-container">
-            <h1 className="message-title">{message.title}</h1>
-            <p className="message-body">{message.body}</p>
-            {/* <Link href={`/posts/${encodeURIComponent(message.id)}`}>
+  return (
+    <div className="all-messages-container">
+      <div className="message-container">
+        <div className="sub-message-container">
+          <h1 className="message-title">{message.title}</h1>
+          <p className="message-body">{message.body}</p>
+          {/* <Link href={`/posts/${encodeURIComponent(message.id)}`}>
                 <a>Comments</a>
             </Link> */}
-        </div></div>
-    )
+        </div>
+      </div>
+    </div>
+  );
 }
