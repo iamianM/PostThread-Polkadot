e6b2e7bdab4ec4dbe98cc5505fcd2d1826ce2aa5
Ian McCann <mcian91@gmail.com>
2022-06-11 15:49:23 -0700

    added styling and pages

--- frontend/src/postthread/components/ListMessages.js
+++ frontend/src/postthread/components/ListMessages.js
@@ -1,33 +1,60 @@
-import React, { useState } from 'react'
-import Message from './Message'
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import Message from "./Message";
+import axios from "axios";
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useQuery } from "react-query";
+
+import "./css/ListMessages.css";
 
 export default function ListMessages() {
-
-  const { error, isError, isLoading } = useQuery('messages', fetchMessages)
-  const [messages, setMessages] = useState([])
+  const { error, isError, isLoading } = useQuery("messages", fetchMessages);
+  // const [allMessages, setAllMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
+  // const messagesPerScroll = 10;
 
   async function fetchMessages() {
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts")
-    setMessages(data)
+    // if (messages.length == 0) {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setMessages(messages.concat(data));
+    // }
+    // if (messages.length + messagesPerScroll >= allMessages.length) {
+    //   const { data } = await axios.get(
+    //     "https://jsonplaceholder.typicode.com/posts"
+    //   );
+    //   setAllMessages(allMessages.concat(data));
+    // }
+    // console.log(allMessages.length);
+    // console.log(messages.length);
+    // setMessages(
+    //   messages.concat(
+    //     allMessages.slice(messages.length, messages.length + messagesPerScroll)
+    //   )
+    // );
+    // console.log(messages.length);
   }
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loadings...</div>;
   }
   if (isError) {
-    return <div>Error! {error.message}</div>
+    console.log("error");
+    return <div>Error! {error.message}</div>;
   }
 
   return (
-    <div>
-      {
-        messages?.map((message) =>
+    <div className="all-messages-container">
+      <InfiniteScroll
+        dataLength={messages.length}
+        next={fetchMessages}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        {messages.map((message) => (
           <Message key={message.id} message={message} />
-        )
-      }
+        ))}
+      </InfiniteScroll>
     </div>
-  )
-
+  );
 }
