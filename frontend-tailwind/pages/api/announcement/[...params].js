2f56b48145a42a1685fbde841c450e36168774de
iamianM <mcian01@gmail.com>
2022-06-28 00:08:56 -0700

    merge

--- frontend-tailwind/pages/api/announcement/[...params].js
+++ frontend-tailwind/pages/api/announcement/[...params].js
@@ -1,23 +1,20 @@
-import { CONSTANTS } from "../../../constants/Constants";
+import { CONSTANTS } from '../../../constants/Constants';
 
 // http://localhost:3000/api/posts/1/10
 
 export default async function handler(req, res) {
-  let headers = new Headers();
-  headers.append("Access-Control-Allow-Origin", "*");
-  const params = req.query.params;
-  if (params.length === 3) {
-    const type = params[0];
-    const iter = params[1];
-    const numberOfMessages = params[2];
-    const data = await fetch(
-      `${CONSTANTS.server}/announcement/${type}/${iter}/${numberOfMessages}?` +
-        new URLSearchParams({ ...req.query }),
-      { headers: headers }
-    );
-    const response = await data.json();
-    res.status(200).json(response);
-    console.log(response);
-    return response;
-  } else res.status(200).json(params);
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', '*');
+    const params = req.query.params
+    if (params.length === 3) {
+        const type = params[0]
+        const iter = params[1]
+        const numberOfMessages = params[2]
+        const data = await fetch(`${CONSTANTS.server}/announcement/${type}/${iter}/${numberOfMessages}?` + new URLSearchParams({ ...req.query }), { headers: headers })
+        const response = await data.json()
+        res.status(200).json(response)
+        console.log(response)
+        return response
+    }
+    else res.status(200).json(params)
 }
