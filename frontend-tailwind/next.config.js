60b783bbf7955a7579a6bbb0ee863f157e4c457a
grignaffini <grignaffinipaolo@gmail.com>
2022-07-07 18:57:12 +0200

    next.config

--- frontend-tailwind/next.config.js
+++ frontend-tailwind/next.config.js
@@ -1,9 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
+if (!process.env.NEXTAUTH_URL) {
+  console.warn(
+    "\x1b[33mwarn",
+    "\x1b[0m",
+    "NEXTAUTH_URL environment variable is not set."
+  );
+  if (process.env.URL) {
+    process.env.NEXTAUTH_URL = process.env.URL;
+    console.warn(
+      "\x1b[33mwarn",
+      "\x1b[0m",
+      `NEXTAUTH_URL environment variable is not set. Using Netlify URL ${process.env.URL}.`
+    );
+  }
 }
 
-// next.config.js
 module.exports = {
-  nextConfig
+  target: "experimental-serverless-trace",
+  future: {
+    webpack5: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
